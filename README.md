80bec623ed2cf10e417280243aa75df6530836c4
SempreBurrasca <gian.comandini@gmail.com>
2018-06-04 09:45:54 +0200

    aggiornamento

---
+++ README.md
@@ -0,0 +1,2 @@
+#DeA Wing clone
+Clone of DeA Wing application (http://www.deagostinigeografia.it/wing/confmondo/confronti.jsp?goal=100077&section=2&year=2018&title=PIL%20totale)
---
+++ js/index.js
@@ -0,0 +1,26 @@
+google.charts.load('current', {
+    'packages':['geochart'],
+    // Note: you will need to get a mapsApiKey for your project.
+    // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+    'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+  });
+  google.charts.setOnLoadCallback(drawRegionsMap);
+
+  function drawRegionsMap() {
+    var data = google.visualization.arrayToDataTable([
+      ['Country', 'Popularity'],
+      ['Germany', 200],
+      ['United States', 300],
+      ['Brazil', 400],
+      ['Canada', 1500],
+      ['France', 600],
+      ['RU', 700],
+      ['Italy', 600]
+    ]);
+
+    var options = {};
+
+    var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+    chart.draw(data, options);
+  }
